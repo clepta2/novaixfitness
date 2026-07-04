cfec64af8404540124a6d5e62a2a59140658debd
clepta2 <clepta2@gmail.com>
2026-07-04 02:35:21 -0300

    fix: SocialFeed reescrito para usar props do pai - posts, likes, comentarios funcionais
    - SocialFeed: componente reescrito para aceitar posts, loading, userId, onLike, onComment
    - SocialFeed: renderiza posts sociais com avatar, conteudo, likes, comentarios
    - SocialFeed: filtro 'all' e 'popular' mantidos
    - social.tsx: agora passa posts, loading, userId, onLike, onComment para SocialFeed

--- app/social.tsx
+++ app/social.tsx
@@ -199,7 +199,7 @@ export default function SocialScreen() {
         <SocialHub badgeCounts={{ feed: 2, challenges: 1 }} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={COLORS.primary} colors={[COLORS.primary]} />}>
           {stories.length > 0 && <StoryRing currentUserId={user?.id} onViewStory={handleViewStory} onAddStory={() => setShowStoryCreate(true)} />}
           <QuickSocialActions onPress={handleQuickAction} />
-          <SocialFeed userId={user?.id} />
+          <SocialFeed posts={posts} loading={loading} userId={user?.id} onLike={handleLike} onComment={handleComment} />
           <WorkoutGroups currentUserId={user?.id} onSelectGroup={handleSelectGroup} />
           <ChallengesList userId={user?.id} />
           <View style={{ marginTop: SPACING.lg }} />
