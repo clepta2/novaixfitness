66056f7c5235145b7556848e0311ea5912c08ed8
clepta2 <clepta2@gmail.com>
2026-07-03 23:07:13 -0300

    fix: StoryRing agora recebe onAddStory prop para evitar crash

--- app/social.tsx
+++ app/social.tsx
@@ -180,7 +180,7 @@ export default function SocialScreen() {
           </TouchableOpacity>
         </View>
         <SocialHub badgeCounts={{ feed: 2, challenges: 1 }} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={COLORS.primary} colors={[COLORS.primary]} />}>
-          {stories.length > 0 && <StoryRing currentUserId={user?.id} onViewStory={handleViewStory} />}
+          {stories.length > 0 && <StoryRing currentUserId={user?.id} onViewStory={handleViewStory} onAddStory={() => setShowStoryCreate(true)} />}
           <QuickSocialActions onPress={handleQuickAction} />
           <SocialFeed posts={posts} loading={loading} currentUserId={user?.id} onLike={handleLike} onComment={handleComment} recentCheckIns={recentCheckIns} />
           <WorkoutGroups currentUserId={user?.id} onSelectGroup={handleSelectGroup} />
