d835f75c9c4f2d0c830e3b10f03c3451e3095c9e
clepta2 <clepta2@gmail.com>
2026-07-03 23:14:10 -0300

    fix: adicionar GradientButton, Stepper, Loading, EmptyState, ErrorDisplay ao barrel de exports UI

--- src/components/exports/ui.ts
+++ src/components/exports/ui.ts
@@ -13,3 +13,8 @@ export { Timer } from '../ui/Timer';
 export { default as AnimatedCounter } from '../ui/AnimatedCounter';
 export { default as FilterBar, getDateRange, PERIOD_FILTERS } from '../ui/FilterBar';
 export { default as BottomTabBar } from '../ui/BottomTabBar';
+export { default as GradientButton } from '../ui/GradientButton';
+export { default as Stepper } from '../ui/Stepper';
+export { default as Loading } from '../ui/Loading';
+export { default as EmptyState } from '../ui/EmptyState';
+export { default as ErrorDisplay } from '../ui/ErrorDisplay';
